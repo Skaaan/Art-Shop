a07027e007552d7629c115294a6dbfc8085b69bb
Luis Maiworm <luis.maiworm@outlook.de>
2023-06-07 12:24:27 +0200

    Added project

---
+++ src/main/java/com/example/cbd/CbdOnlineshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.cbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbdOnlineshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbdOnlineshopApplication.class, args);
+	}
+
+}
